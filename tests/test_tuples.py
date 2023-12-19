ad99f4642538f44c0a050d9dec5a830ba744d1be
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-19 21:27:00 +0000

    Add tuples chapter

---
+++ tests/test_tuples.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+class TestTuples:
+    def test_assign_item(self):
+        """
+        Given a tuple of items
+        When another item is assigned to the tuple
+        Then a `TypeError` is raised
+        """
+        # Given
+        items = "a", "b", "c"
+
+        # When / Then
+        items[3] = "d"
+
+    def test_raise_error_when_item_is_assigned_after_instantiation(self):
+        """
+        Given a tuple of items
+        When another item is assigned to the tuple
+        Then a `TypeError` is raised
+        """
+        # Given
+        items = "a", "b", "c"
+
+        # When / Then
+        with pytest.raises(TypeError):
+            items[3] = "d"
