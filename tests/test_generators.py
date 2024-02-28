c04540b672c5ffcf86f3230353369039b71dcf59
A-Ashiq <afaan.ashiq@nate.tech>
2024-02-28 21:40:38 +0000

    Add test for exhausting a generator

---
+++ tests/test_generators.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+class TestGenerators:
+    def test_items_are_yielded_until_exhausted(self):
+        """
+        Given an iterator of integers
+        When `next()` is called continuously
+        Then the next integers are returned
+            until the iterator is exhausted
+        """
+        # Given
+        iterator = (n for n in range(3))
+
+        # When
+        first_value = next(iterator)
+        assert first_value == 0
+
+        second_value = next(iterator)
+        assert second_value == 1
+
+        third_value = next(iterator)
+        assert third_value == 2
+
+        # Then
+        with pytest.raises(StopIteration):
+            next(iterator)
