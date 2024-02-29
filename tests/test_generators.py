cb7d57d1ce18284744b504b6f71c04a701314be3
A-Ashiq <afaan.ashiq@nate.tech>
2024-02-29 20:18:06 +0000

    Add test for exhausting an iterator function making use of the `yield` keyword

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -25,3 +25,31 @@ class TestGenerators:
         # Then
         with pytest.raises(StopIteration):
             next(iterator)
+
+    def test_items_are_yielded_until_exhausted_from_function(self):
+        """
+        Given an iterator of integers as a function
+        When `next()` is called continuously
+        Then the next integers are returned
+            until the iterator is exhausted
+        """
+        # Given
+        def example_iterator():
+            for i in range(3):
+                yield i
+
+        iterator = example_iterator()
+
+        # When
+        first_value = next(iterator)
+        assert first_value == 0
+
+        second_value = next(iterator)
+        assert second_value == 1
+
+        third_value = next(iterator)
+        assert third_value == 2
+
+        # Then
+        with pytest.raises(StopIteration):
+            next(iterator)
