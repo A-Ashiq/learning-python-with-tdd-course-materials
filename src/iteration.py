940520d73582b4a1852db747e5ee13ab552f33de
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-06 22:47:07 +0000

    Add section for iteration

---
+++ src/iteration.py
@@ -0,0 +1,3 @@
+def print_all_items(items):
+    for item in items:
+        print(item)
---
+++ tests/test_iteration.py
@@ -0,0 +1,27 @@
+from src.iteration import print_all_items
+
+
+class TestIteration:
+    def test_loops_over_list(self):
+        """
+        Given a list of strings
+        When `print_all_items()` is called
+        Then each item is printed
+        """
+        # Given
+        items = ["red", "blue", "green"]
+
+        # When / Then
+        print_all_items(items=items)
+
+    def test_loops_over_dict(self):
+        """
+        Given a dict of strings
+        When `print_all_items()` is called
+        Then each item is printed
+        """
+        # Given
+        items = {"red": 1, "blue": 2, "green": 3}
+
+        # When / Then
+        print_all_items(items=items)
