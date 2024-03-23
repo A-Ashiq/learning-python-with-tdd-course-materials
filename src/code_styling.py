6d6c982059aa8f202fdb2541c4770a4eed4bca6c
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-23 10:45:46 +0000

    Add section for `code_styling` showing impact of applying `black`

---
+++ src/code_styling.py
@@ -0,0 +1,18 @@
+def some_function(first_number, second_number, third_number, fourth_number, fifth_number, sixth_number, seventh_number):
+
+    denominator = [fourth_number, fifth_number, sixth_number, seventh_number]
+
+# Function names are the same.
+# This is to demonstrate the `black` code formatter rewriting the above function.
+
+def some_function(
+    first_number,
+    second_number,
+    third_number,
+    fourth_number,
+    fifth_number,
+    sixth_number,
+    seventh_number,
+):
+
+    denominator = [fourth_number, fifth_number, sixth_number, seventh_number]
